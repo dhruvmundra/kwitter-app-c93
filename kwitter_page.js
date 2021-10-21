e7840e1a86d42251b0827b1b2746b8e36ceba021
dhruvmundra <81858843+dhruvmundra@users.noreply.github.com>
2021-10-21 16:15:17 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,15 +1,17 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCYvbfCvtf_GWWg0DmbNTWfNxXrcEj-egI",
-    authDomain: "kwitter-app-ab873.firebaseapp.com",
-    databaseURL: "https://kwitter-app-ab873-default-rtdb.firebaseio.com",
-    projectId: "kwitter-app-ab873",
-    storageBucket: "kwitter-app-ab873.appspot.com",
-    messagingSenderId: "496979231404",
-    appId: "1:496979231404:web:a01acf7eb12112573f9509",
-    measurementId: "G-6E7LYDP0ML"
-  };
-  firebase.intializeApp(firebaseConfig );
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDVVSPxUHwGjfNHtidcVu8Obpd66b0fFeQ",
+  authDomain: "kwitter-93a5b.firebaseapp.com",
+  databaseURL: "https://kwitter-93a5b-default-rtdb.firebaseio.com",
+  projectId: "kwitter-93a5b",
+  storageBucket: "kwitter-93a5b.appspot.com",
+  messagingSenderId: "622153470595",
+  appId: "1:622153470595:web:3de540143e983df0ae41b6",
+  measurementId: "G-GH5WEFG7BR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   user_name=localStorage.getItem("user_name");
   room_name=localStorage.getItem("room_name");
   
